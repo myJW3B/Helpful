064cfa9c54c76dce14b5c44c685147d422b986a3
JWB <john.skem9@gmail.com>
2024-07-04 14:13:49 -0400

    Added the zip() finction to zip files.

--- erday/Helpful_Files.php
+++ erday/Helpful_Files.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace JW3B\erday;
+use ZipArchive;
+use RecursiveIteratorIterator;
+use RecursiveDirectoryIterator;
 
 class Helpful_Files {
 	public static function mk_dir_writable($dir){
@@ -116,4 +119,38 @@ class Helpful_Files {
 		return $file_ary;
 	}
 
-}
+// http://stackoverflow.com/questions/1334613/how-to-recursively-zip-a-directory-in-php
+// zip('/folder/to/compress/', './compressed.zip');
+		/**
+	 * Zip function will
+	 *
+	 * @param string $source
+	 * 		A directory of files to zip
+	 * @param string $destination
+	 * 		The path/to/file.zip to put the zip file
+	 * @return bool
+	 */
+	public function zip($source, $destination){
+		if(!extension_loaded('zip') || !file_exists($source)){ return false; }
+		$zip = new ZipArchive();
+		if(!$zip->open($destination, ZIPARCHIVE::CREATE)){ return false; }
+		$source = str_replace('\\', '/', realpath($source));
+		if(is_dir($source) === true){
+			$files = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($source), RecursiveIteratorIterator::SELF_FIRST);
+			foreach ($files as $file){
+				$file = str_replace('\\', '/', $file);
+				// Ignore "." and ".." folders
+				if( in_array(substr($file, strrpos($file, '/')+1), array('.', '..')) )
+						continue;
+				$file = realpath($file);
+				if (is_dir($file) === true){
+						$zip->addEmptyDir(str_replace($source . '/', '', $file . '/'));
+				} else if (is_file($file) === true){
+						$zip->addFromString(str_replace($source . '/', '', $file), file_get_contents($file));
+				}
+			}
+		} elseif (is_file($source) === true){
+			$zip->addFromString(basename($source), file_get_contents($source));
+		}
+		return $zip->close();
+	}
